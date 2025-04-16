24aadefb6f3b4a5ab2a64f961fc540260f3b7d6c
logan <huang1002788835@outlook.com>
2025-04-16 16:46:48 -0700

    complete Merger.go

---
+++ pkg/internel/Merger.go
@@ -0,0 +1,67 @@
+package internel
+
+import (
+	"bufio"
+	"container/heap"
+	"os"
+)
+
+type Merger struct {
+	tempFiles  []string
+	outputFile string
+}
+
+func (m *Merger) Merge() error {
+	// assign a Reader to every tempFile
+	readers := make([]*bufio.Scanner, len(m.tempFiles))
+	files := make([]*os.File, len(m.tempFiles))
+
+	for i, temFile := range m.tempFiles{
+		file, err := os.Open(temFile)
+		if err != nil{
+			return err
+		}
+		files[i] = file
+		readers[i] = bufio.NewScanner(file)
+	}
+
+	defer func ()  {
+		for _ , f := range files{
+			f.Close()
+		}
+	}()
+
+	// Initialize Heap
+	h := &MinHeap{}
+	heap.Init(h)
+
+	for i, reader := range readers{
+		if reader.Scan(){
+			heap.Push(h, HeapItem{value: reader.Text(), fileIndex: i})
+		}
+	}
+
+	output, err := os.Create(m.outputFile)
+	if  err != nil {
+		return err
+	}
+	defer output.Close()
+
+	writer := bufio.NewWriter(output)
+
+	for h.Len() > 0{
+		item := heap.Pop(h).(HeapItem)
+
+		_, err := writer.WriteString(item.value + "\n")
+		if err != nil {
+			return err
+		}
+
+		if readers[item.fileIndex].Scan() {
+			heap.Push(h, HeapItem{value: readers[item.fileIndex].Text(), fileIndex: item.fileIndex})
+		}
+	}
+
+	writer.Flush()
+	return nil
+}
